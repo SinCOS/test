d570d1d06e04efdf2e6b2711d98196a09a8a4c03
qyf1994 <qyf1994@gmail.com>
2018-09-22 15:29:01 +0800

    fix user

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'mobile', 'password',
+        'name', 'mobile', 'password','vip'
     ];
 
     /**
