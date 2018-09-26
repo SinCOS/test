a029aaae8dc74f96d3aa02caf6408eb600abe0b4
qyf1994 <qyf1994@gmail.com>
2018-09-26 18:28:54 +0800

    fix url

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -11,6 +11,22 @@ use Encore\Admin\Layout\Row;
 
 class HomeController extends Controller
 {
+
+    public static function first()
+    {
+        $envs = [
+            ['name' => '会员待审核',       'value' => \App\User::where('status','=',0)->sum(),
+            ['name' => '会员数',   'value' => \App\User::where('status','=',1)->sum()],
+            ['name' => 'VIP数量',               'value' => \App\User::where('vip','>=',\Carbon\Carbon::now())->sum()],
+            ['name' => '众筹项目数',             'value' => \App\DC::sum()],
+            ['name' => '投标中',            'value' => \App\DC::where('status','=',1)->sum()],
+
+            ['name' => '成功项目数',      'value' => \App\DC::where('status','=',2)->sum()],
+            ['name' => '成功筹款',    'value' => \App\vOrder::sum()],
+        ];
+
+        return view('admin::dashboard.environment', compact('envs'));
+    }
     public function index()
     {
         return Admin::content(function (Content $content) {
@@ -23,7 +39,7 @@ class HomeController extends Controller
             $content->row(function (Row $row) {
 
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                    $column->append(self::first());
                 });
 
                 $row->column(4, function (Column $column) {
