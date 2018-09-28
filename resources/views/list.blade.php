3368e6b3ab387defc3ace520e0cad688e6caff5d
qyf1994 <qyf1994@gmail.com>
2018-09-28 16:56:09 +0800

    fix url

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,8 +5,9 @@
     <div class="row">
     	<div class="col-md-12">
     		<ul class="list-group">
+    				 <li class="list-group-item active">我的众筹</li>
     			@foreach ($arrays as $element)
-    				<li class="list-group-item"><a href="/detail/{{$element->id}}"> {{$element->dyxx}} </a></li>
+    				 <li class="list-group-item"><a href="/detail/{{$element->id}}"> {{$element->dyxx}}  <span class='pull-right'> ￥{{$element->jkje}} </span></a></li>
     			@endforeach
 			</ul>
     	</div>
